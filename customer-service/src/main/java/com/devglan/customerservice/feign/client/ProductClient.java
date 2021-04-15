ea7ce1b42bef43dadabc8221b8246e33cc8e271c
JeyaSekar <sx00563172@techmahindra.com>
2021-04-15 21:09:48 +0530

    Update ProductClient.java
    updated with RequestLine(value = "GET /products/customer/{custId}")

--- customer-service/src/main/java/com/devglan/customerservice/feign/client/ProductClient.java
+++ customer-service/src/main/java/com/devglan/customerservice/feign/client/ProductClient.java
@@ -1,6 +1,9 @@
 package com.devglan.customerservice.feign.client;
 
 import com.devglan.customerservice.feign.config.CustomFeignConfig;
+
+import feign.RequestLine;
+
 import com.devglan.commons.Product;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,15 +16,19 @@ import java.util.List;
 @FeignClient(name="product-service", configuration = CustomFeignConfig.class)
 public interface ProductClient {
 
-    @GetMapping("/products")
+   // @GetMapping("/products")
+    @RequestLine(value = "GET /products")
     List<Product> listProducts();
 
-    @GetMapping("/products/{id}")
+    //@GetMapping("/products/{id}")
+    @RequestLine(value = "GET /products/{id}")
     Product getProductById(@PathVariable String id);
 
-    @PostMapping("/products")
-    Product create(@RequestBody Product product);
+	/*
+	 * @PostMapping("/products") Product create(@RequestBody Product product);
+	 */
 
-    @GetMapping("/products/customer/{custId}")
+   // @GetMapping("/products/customer/{custId}")
+    @RequestLine(value = "GET /products/customer/{custId}")
     List<Product> listProductsByCustomerId(@PathVariable String custId);
 }
